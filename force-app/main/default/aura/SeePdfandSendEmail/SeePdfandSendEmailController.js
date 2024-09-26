cf4c882fcdf7650e29ea1db7eef2b4a25e839dd2
Syed-Fozan <94220076+Syed-Fozan@users.noreply.github.com>
2024-09-27 01:19:21 +0500

    Weekly Accountability Report

---
+++ force-app/main/default/aura/SeePdfandSendEmail/SeePdfandSendEmailController.js
@@ -0,0 +1,6 @@
+({
+    generatePDFs: function(component, event, helper) {
+    // helper.generatePDF(component,event,helper);
+    helper.sendEmailWithPDF(component,event,helper);
+    }
+})
---
+++ force-app/main/default/aura/SeePdfandSendEmail/SeePdfandSendEmailHelper.js
@@ -0,0 +1,58 @@
+({
+    sendEmailWithPDF : function(component, event, helper) {
+        var action = component.get("c.sendEmailWithPDF");
+        action.setParams({
+            recordId: component.get("v.recordId")
+        });
+
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var result = response.getReturnValue();
+                if(result === 'Success') {
+                    this.showToastMessage('Email Sent Successfully ', 'Success');
+                } else {
+                    alert("Error: " + result);
+                }
+            } else {
+                alert("Failed with state: " + state);
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    generatePDF: function(component, event, helper) {
+        var recordId = component.get("v.recordId");
+        var action = component.get("c.generatePDF");
+        action.setParams({
+            "recordId": recordId
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var result = response.getReturnValue();
+                if (result) {
+                    window.open(result, '_blank');
+                } else {
+                    console.log('No URL returned from Apex method');
+                }
+            } else {
+                console.log('Error calling Apex method: ' + state);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    showToastMessage: function(message, type) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            duration: ' 5000',
+            mode: 'dismissible',
+            message: message,
+            type: type,
+            title: type ,
+        });
+        toastEvent.fire();
+    }
+})
